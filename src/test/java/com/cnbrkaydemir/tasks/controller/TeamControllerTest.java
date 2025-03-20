80e445b92e56faaa2765b0b568f09a5287daccf5
cnbrkaydemir <canberk.aydemir@yahoo.com>
2025-03-20 22:42:58 +0300

    Write unit tests for team controller functions.

--- src/test/java/com/cnbrkaydemir/tasks/controller/TeamControllerTest.java
+++ src/test/java/com/cnbrkaydemir/tasks/controller/TeamControllerTest.java
@@ -1,11 +1,17 @@
 package com.cnbrkaydemir.tasks.controller;
 
-import com.cnbrkaydemir.tasks.dto.CreateTeamDto;
-import com.cnbrkaydemir.tasks.dto.TeamDto;
+import com.cnbrkaydemir.tasks.dto.*;
 import com.cnbrkaydemir.tasks.exception.GlobalExceptionHandler;
 import com.cnbrkaydemir.tasks.exception.notfound.TeamNotFoundException;
+import com.cnbrkaydemir.tasks.exception.notfound.UserNotFoundException;
+import com.cnbrkaydemir.tasks.factory.DepartmentTestDataFactory;
+import com.cnbrkaydemir.tasks.factory.ProjectTestDataFactory;
 import com.cnbrkaydemir.tasks.factory.TeamTestDataFactory;
+import com.cnbrkaydemir.tasks.factory.UsersTestDataFactory;
+import com.cnbrkaydemir.tasks.model.Department;
+import com.cnbrkaydemir.tasks.model.Project;
 import com.cnbrkaydemir.tasks.model.Team;
+import com.cnbrkaydemir.tasks.model.Users;
 import com.cnbrkaydemir.tasks.service.TeamService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +27,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -151,6 +158,119 @@ public class TeamControllerTest {
 
         String apiPath = BASE_PATH + "/v1/" +randomId;
         mockMvc.perform(delete(apiPath))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void findTeamUsers_ShouldReturnOk() throws Exception {
+        when(teamService.getTeamUsers(team.getId()))
+                .thenReturn(Stream.of(UsersTestDataFactory.createDefaultUser())
+                        .map(user -> modelMapper.map(user, UserDto.class))
+                        .toList());
+
+        String apiPath = BASE_PATH + "/v1/"+team.getId()+"/users";
+        mockMvc.perform(get(apiPath))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findTeamUsers_ShouldReturnNotFound() throws Exception {
+        UUID randomId = UUID.randomUUID();
+        when(teamService.getTeamUsers(randomId))
+                .thenThrow(new TeamNotFoundException(randomId));
+
+        String apiPath = BASE_PATH + "/v1/"+randomId+"/users";
+        mockMvc.perform(get(apiPath))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void findTeamProject_ShouldReturnOk() throws Exception {
+        Project project = ProjectTestDataFactory.createDefaultProject();
+        project.setTeams(List.of(team));
+        when(teamService.getTeamProject(team.getId()))
+                .thenReturn(modelMapper.map(project, ProjectDto.class));
+
+        String apiPath = BASE_PATH + "/v1/"+team.getId()+"/project";
+        mockMvc.perform(get(apiPath))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findTeamProject_ShouldReturnNotFound() throws Exception {
+        UUID randomId = UUID.randomUUID();
+        when(teamService.getTeamProject(randomId))
+                .thenThrow(new TeamNotFoundException(randomId));
+
+        String apiPath = BASE_PATH + "/v1/"+randomId+"/project";
+        mockMvc.perform(get(apiPath))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void findTeamDepartment_ShouldReturnOk() throws Exception {
+        Department department = DepartmentTestDataFactory.createDefaultDepartment();
+        department.setTeams(List.of(team));
+        when(teamService.getTeamDepartment(team.getId()))
+                .thenReturn(modelMapper.map(department, DepartmentDto.class));
+
+        String apiPath = BASE_PATH + "/v1/"+team.getId()+"/department";
+        mockMvc.perform(get(apiPath))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findTeamDepartment_ShouldReturnNotFound() throws Exception {
+        UUID randomId = UUID.randomUUID();
+        when(teamService.getTeamDepartment(randomId))
+                .thenThrow(new TeamNotFoundException(randomId));
+
+        String apiPath = BASE_PATH + "/v1/"+randomId+"/department";
+        mockMvc.perform(get(apiPath))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void addUserToTeam_ShouldReturnOk() throws Exception {
+        Users user = UsersTestDataFactory.createDefaultUser();
+        when(teamService.addUserToTeam(team.getId(), user.getId()))
+                .thenReturn(teamDto);
+
+        String apiPath = BASE_PATH + "/v1/"+team.getId()+"/user/"+user.getId();
+        mockMvc.perform(post(apiPath))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void addUserToTeam_ShouldReturnNotFound() throws Exception {
+        UUID randomId = UUID.randomUUID();
+        when(teamService.addUserToTeam(team.getId(), randomId))
+                .thenThrow(new UserNotFoundException(randomId));
+
+        String apiPath = BASE_PATH + "/v1/"+team.getId()+"/user/"+randomId;
+        mockMvc.perform(post(apiPath))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void discardUserFromTeam_ShouldReturnOk() throws Exception {
+        Users user = UsersTestDataFactory.createDefaultUser();
+        when(teamService.discardUserFromTeam(team.getId(), user.getId()))
+                .thenReturn(teamDto);
+
+        String apiPath = BASE_PATH + "/v1/"+team.getId()+"/user/"+user.getId()+"/discard";
+        mockMvc.perform(post(apiPath))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void discardUserFromTeam_ShouldReturnNotFound() throws Exception {
+        UUID randomId = UUID.randomUUID();
+        when(teamService.discardUserFromTeam(team.getId(), randomId))
+                .thenThrow(new UserNotFoundException(randomId));
+
+        String apiPath = BASE_PATH + "/v1/"+team.getId()+"/user/"+randomId+"/discard";
+        mockMvc.perform(post(apiPath))
                 .andExpect(status().isNotFound());
     }
 
